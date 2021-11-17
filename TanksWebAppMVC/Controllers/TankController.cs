597af94f9af1131248948e831b708ada79ee68b2
Åsa Jonsson <asaj71@gmail.com>
2021-11-17 14:19:57 +0100

    Add project files.

---
+++ TanksWebAppMVC/Controllers/TankController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TanksWebAppMVC.Controllers
+{
+    public class TankController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult TankHistory()
+        {
+            return View();
+        }
+    }
+}
